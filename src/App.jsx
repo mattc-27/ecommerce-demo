8c5ed833c6cd62a83136aba8b09ba6d244a374ee
mattc-27 <mattcopeland927@gmail.com>
2023-12-28 12:49:41 -0700

    test config

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import './style.css'
+import logoLarge  from './logoLarge.jpg';
+
+import './style.css';
 
 
 export default function App() {
 
     return (
-        <div className='main'>
-            <div className='title'>
-                <h1>Hello world</h1>
+        <>
+            <header>
+                <div className='header-content'>
+                    <div className='header-logo'>
+                        <h2>PurRoots</h2>
+                    </div>
+                    <nav>
+                        <a className='nav-link' href='/'>Home</a>
+
+                    </nav>
+                </div>
+            </header>
+            <div className='main'>
+                <div className='title m-2' style={{border: '1px solid #fff'}}>
+                    <h1>Hello world</h1>
+                    <img style={{height: '50%', margin: '2%'}} src={logoLarge} />
+                </div>
+                <Wrapper />
             </div>
-        </div>
+        </>
     );
 }
